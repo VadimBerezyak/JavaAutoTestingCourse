2c28c8ede4e99c63206e2537e7aa125e5ecb15b6
VadimBerezyak <89918671+VadimBerezyak@users.noreply.github.com>
2022-02-13 11:51:59 +0300

    Homework 2. Add Task 2 "Sum of the "nett" salary is ..."

---
+++ src/berezyk/vadim/Main1.java
@@ -0,0 +1,9 @@
+package berezyk.vadim;
+
+public class Main1 {
+    public static void main(String[] args) {
+        float gross = 70010f;
+        float nett = gross*0.87f;
+        System.out.println("Gross salary is "+ nett+ " rubles");
+    }
+}
