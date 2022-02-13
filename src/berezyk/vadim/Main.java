ecc6d476d88e0a6f7d3605c33aad8e021f4d3580
VadimBerezyak <89918671+VadimBerezyak@users.noreply.github.com>
2022-02-13 11:43:04 +0300

    Homework 2. Task 1 "Cost of fuel"

---
+++ src/berezyk/vadim/Main.java
@@ -0,0 +1,15 @@
+package berezyk.vadim;
+
+public class Main {
+
+    public static void main(String[] args) {
+        float priceFuel = 53.55f;
+        float volumeFuel = 45f;
+        float cost = priceFuel*volumeFuel;
+        System.out.println("You should pay "+ cost + " rubles");
+
+    }
+
+
+
+}
